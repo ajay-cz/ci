c6ddaeddce05f56706e554745d674a7e1a910005
Mock_Arch <wellganesh@gmail.com>
2018-06-08 16:30:58 +0530

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -1,9 +1,13 @@
 import math
 import unittest
+from urllib.request import Request, urlopen
 
 
 
 class SimpleFlaskTest(unittest.TestCase):
-  flag = False
-  def test_flask(self):
-		self.assertTrue(flag,msg="the api json response it not fine")
+	req = Request("http://0.0.0.0:5000/")
+	response = urlopen(req)
+	flag = False
+	def test_flask(self):
+		print(self.__class__.response)
+		self.assertTrue(self.__class__.flag,msg="the api json response it not fine")
