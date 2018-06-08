49d97a276c0af05f50c8e59e18556e01c40860b1
Mock_Arch <wellganesh@gmail.com>
2018-06-08 16:21:57 +0530

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -6,5 +6,4 @@ import unittest
 class SimpleFlaskTest(unittest.TestCase):
   flag = False
   def test_flask(self):
-		self.assertTrue(flag,
-						msg="the api json response it not fine")
+		self.assertTrue(flag,msg="the api json response it not fine")
